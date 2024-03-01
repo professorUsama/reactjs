d1a76779649d6bd1bb646cb1876299cbcb57d269
Professor Usama <149573452+usamaamjid@users.noreply.github.com>
2024-03-02 02:00:29 +0500

    update

--- mini-context/src/App.jsx
+++ mini-context/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login';
+import Profile from './components/Profile'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    < UserContextProvider>
+      <Login />
+      <Profile />
+    </ UserContextProvider>
   )
 }
 
